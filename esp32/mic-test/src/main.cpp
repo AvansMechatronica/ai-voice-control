bbf20e50cd16e817deb9a8f97c7f2ca84341b8c6
Gerard Harkema <GerardAnneHarkema@gmail.com>
2026-03-21 11:27:54 +0100

    20260321

---
+++ esp32/mic-test/src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <driver/i2s.h>
+
+
+
+#define SAMPLE_RATE 16000
+
+void setup() {
+  Serial.begin(115200);
+
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+    .communication_format = I2S_COMM_FORMAT_I2S,
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = 64
+  };
+
+  i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = I2S_PIN_NO_CHANGE,
+    .data_in_num = I2S_SD
+  };
+
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_PORT, &pin_config);
+}
+
+void loop() {
+  int32_t sample;
+  size_t bytes_read;
+
+  i2s_read(I2S_PORT, &sample, sizeof(sample), &bytes_read, portMAX_DELAY);
+
+  // schaal terug naar 16-bit (optioneel)
+  int16_t audio = sample >> 14;
+
+  Serial.println(audio);
+}
